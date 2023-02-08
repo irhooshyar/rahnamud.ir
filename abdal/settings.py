fbcd807c3ddae9bc70b7008f4d5099b6c7692dd2
irhooshyar <123286611+irhooshyar@users.noreply.github.com>
2023-02-08 12:25:21 +0330

    email reset password

--- abdal/settings.py
+++ abdal/settings.py
@@ -199,3 +199,12 @@ if not (os.environ.get('EMAIL_HOST') is None):
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'mail.irhooshyar.com'
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+# EMAIL_HOST_USER = 'info@irhooshyar.com'
+# EMAIL_HOST_PASSWORD = 'Grhf5)368&6dghjbvd366436'
