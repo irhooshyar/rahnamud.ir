a5406067fbd4f43c400274e6b0708256c696d9ea
MojtabaAbtahi <36766550+MojtabaAbtahi@users.noreply.github.com>
2023-02-09 14:08:07 +0330

    comment persian auto

--- scripts/StratAutomating.py
+++ scripts/StratAutomating.py
@@ -1,25 +1,25 @@
-from scripts.Persian import PersianAutomating
-# from scripts.English import EnglishAutomating
-# from scripts.Russia import RussiaAutomating
+# from scripts.Persian import PersianAutomating
+# # from scripts.English import EnglishAutomating
+# # from scripts.Russia import RussiaAutomating
 
-import after_response
+# import after_response
 
-@after_response.enable
-def apply(folder_name, Country, tasks_list,host_url):
-    try:
-        if Country.language == "فارسی":
-            PersianAutomating.persian_apply(folder_name, Country, tasks_list, host_url)
+# @after_response.enable
+# def apply(folder_name, Country, tasks_list,host_url):
+#     try:
+#         if Country.language == "فارسی":
+#             PersianAutomating.persian_apply(folder_name, Country, tasks_list, host_url)
 
-        elif Country.language == "کتاب":
-            PersianAutomating.persian_apply(folder_name, Country, tasks_list, host_url)
+#         elif Country.language == "کتاب":
+#             PersianAutomating.persian_apply(folder_name, Country, tasks_list, host_url)
 
-        elif Country.language == "استاندارد":
-            PersianAutomating.persian_apply(folder_name, Country, tasks_list, host_url)
+#         elif Country.language == "استاندارد":
+#             PersianAutomating.persian_apply(folder_name, Country, tasks_list, host_url)
 
-        Country.status = "Done"
-        Country.save()
+#         Country.status = "Done"
+#         Country.save()
 
-    except Exception as e:
-        print("Error:" + str(e))
-        Country.status = "Error: " + str(e)
-        Country.save()
+#     except Exception as e:
+#         print("Error:" + str(e))
+#         Country.status = "Error: " + str(e)
+#         Country.save()
