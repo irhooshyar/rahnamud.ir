083d92a1337fc05b59491855b6013cf227a2cedb
Parinaz <parisoltan77@gmail.com>
2023-02-12 17:09:27 +0330

    email changes

--- abdal/settings.py
+++ abdal/settings.py
@@ -193,7 +193,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.irhooshyar.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'info@irhooshyar.com'
+EMAIL_HOST_USER = 'support@irhooshyar.com'
 EMAIL_HOST_PASSWORD = 'Grhf5)368&6dghjbvd366436'
 if not (os.environ.get('EMAIL_HOST') is None):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
