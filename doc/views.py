f48e477c4fd99227c035a99e663b7091648d757e
irhooshyar <123286611+irhooshyar@users.noreply.github.com>
2023-02-26 13:36:43 +0330

    capcha OK

--- doc/views.py
+++ doc/views.py
@@ -3590,6 +3590,7 @@ def signup_user_activation(request, email, code):
 def SaveUser(request, firstname, lastname, email, phonenumber, role, username, password, ip, expertise):
     print(username)
     user_username = User.objects.filter(username=username)
+    print(user_username)
     user_email = User.objects.filter(email=email)
     if user_username.count() > 0:
         return JsonResponse({"status": "duplicated username"})
